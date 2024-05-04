245f0ec2634a4b4b141fd8ac783d26e1e769aeb6
Tony <tonyyu1129@gmail.com>
2024-05-03 19:33:58 -0700

    set up landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Header from "./features/header/Header";
 import useViewport from "./hooks/useViewport";
+import Landing from "./views/landing/Landing";
 
 export const APP_ID = "app_id";
 
@@ -10,6 +11,7 @@ function App() {
   return (
     <main id={APP_ID}>
       <Header />
+      <Landing />
     </main>
   );
 }
---
+++ src/views/landing/Landing.tsx
@@ -0,0 +1,5 @@
+import "./landing.css";
+
+export default function Landing() {
+  return <div className="LANDING"></div>;
+}
