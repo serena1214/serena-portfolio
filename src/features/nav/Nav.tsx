625d23ba595f8192303f4935e027a80f481b3227
Serena Hou <serena1214@gmail.com>
2024-05-03 17:08:40 -0700

    created a nav component

---
+++ src/features/nav/Nav.tsx
@@ -0,0 +1,25 @@
+import "./nav.css";
+
+type Link = {
+  label: string;
+  id: string;
+};
+const links: Link[] = [
+  { label: "about", id: "about_id" },
+  { label: "project", id: "project_id" },
+  { label: "visual", id: "visual_id" },
+  { label: "video", id: "video_id" },
+  { label: "contact", id: "contact_id" },
+];
+
+export default function Nav() {
+  return (
+    <nav className="NAV">
+      <ul>
+        {links.map((l) => {
+          return <li key={l.label}>{l.label}</li>;
+        })}
+      </ul>
+    </nav>
+  );
+}
